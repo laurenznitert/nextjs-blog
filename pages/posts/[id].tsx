8ea1a6575350c066dd52799b04a65e015396cb6c
Laurenz Nitert <laurenz.nitert@ecare.nl>
2023-08-25 21:13:19 +0200

    magweg

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -15,15 +15,20 @@ export default function Post({ postData }: PostProps) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
+            <Layout>
+                <Head>
+                    <title>{postData.title}</title>
+                </Head>
+                {postData.title}
+                <br />
+                {postData.id}
+                <br />
+                <Date dateString={postData.date} />
+                <br />
                 <div
                     dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
                 />
-            </article>
+            </Layout>
         </Layout>
     );
 }
